9ef0183900e0c8df015ab162f44e7566c9239ef9
whatsGravity <rancic.minya@gmail.com>
2016-07-08 11:11:23 -0400

    Added crc algorithm(not working) Have a go to position function.

---
+++ robotiq.py
@@ -0,0 +1,96 @@
+# robotiq.py
+import serial
+import time
+import binascii
+import sys
+
+speed = 0;
+force = 0;
+position = 0;
+ser = 0
+
+def appendCrc(message):
+    n = len(message)
+    crc = int("ffff", 16)
+    polynomial = int("a001", 16)
+
+    for i in range(0, n):
+        crc = crc ^ message[i]
+        for j in range(1, 8):
+            if crc & 1:
+                crc = crc >> 1
+                crc = crc ^ polynomial
+            else:
+                crc = crc >> 1
+    lowByte = crc & int("ff", 16)
+    highByte = (crc & int("ff00", 16)) >> 8
+
+    print(message)
+    print(lowByte, highByte)
+    amsg = message + str(lowByte) + str(highByte)
+    return amsg
+
+
+def init():
+    # type: () -> object
+    reload(sys)
+    sys.setdefaultencoding('utf-8')
+    global ser
+    ser = serial.Serial(port='COM9', baudrate=115200, timeout=1,
+                        parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS)
+    counter = 0
+    while counter < 1:
+        counter = counter + 1
+        ser.write(binascii.unhexlify("091003E80003060000000000007330"))
+        data_raw = ser.readline()
+        print(data_raw)
+        data = binascii.hexlify(data_raw)
+        print ("Response 1 ", data)
+        time.sleep(0.01)
+
+        ser.write(binascii.unhexlify("090307D0000185CF"))
+        data_raw = ser.readline()
+        print(data_raw)
+        data = binascii.hexlify(data_raw)
+        print ("Response 2 ", data)
+        time.sleep(1)
+    return ser
+
+def demo():
+    #ser = init()
+    while (True):
+        print ("Close gripper")
+        ser.write(binascii.unhexlify("091003E8000306090000FFFFFF4229"))
+        data_raw = ser.readline()
+        print(data_raw)
+        data = binascii.hexlify(data_raw)
+        print ("Response 3 ", data)
+        time.sleep(2)
+
+        print ("Open gripper")
+        ser.write(binascii.unhexlify("091003E800030609000000FFFF7219"))
+        data_raw = ser.readline()
+        print(data_raw)
+        data = binascii.hexlify(data_raw)
+        print ("Response 4 ", data)
+        time.sleep(2)
+
+def setPosition(pos):
+    if(pos < 0 or pos > 255):
+        return 'Error: Position must be between 0 and 255'
+    strpos = hex(pos)[2:]
+    while(len(strpos) < 2):
+        strpos = "0" + strpos
+
+    print(strpos)
+    #strpos = "FFFFFF"
+    ser.write(binascii.unhexlify("091003E8000306090000" + strpos + "FFFF7219"))
+    data_raw = ser.readline()
+    print(data_raw)
+    data = binascii.hexlify(data_raw)
+    print ("Response 4 ", data)
+    time.sleep(2)
+
+init()
+setPosition(0)
+print appendCrc([9, 16, 3, 232, 0, 3, 6, 1, 0, 0, 0, 0, 0])
